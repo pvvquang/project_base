8e916fb6f8b5a72cb6853457d44d6c45d24427ec
PvQ <pvq@PvQs-MacBook-Pro.local>
2023-11-05 09:30:50 +0700

    RN: change icon and splash screen

--- ReactNative/android/app/src/main/java/com/reactnative/MainActivity.java
+++ ReactNative/android/app/src/main/java/com/reactnative/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 import org.devio.rn.splashscreen.SplashScreen;
 
 public class MainActivity extends ReactActivity {
@@ -29,12 +30,6 @@ public class MainActivity extends ReactActivity {
         getMainComponentName(),
         // If you opted-in for the New Architecture, we enable the Fabric Renderer.
         DefaultNewArchitectureEntryPoint.getFabricEnabled());
-  }
-
-  //splash screen
-  @Override
-  protected String getMainComponentName() {
-    return "MySplashScreen";
   }
   
   @Override
